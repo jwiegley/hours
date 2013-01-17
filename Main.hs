881961f6342d9f4e07fc9b93b4c016d50bee4e78
John Wiegley <johnw@fpcomplete.com>
2013-01-17 02:21:05 -0600

    Several more fixes

--- Main.hs
+++ Main.hs
@@ -3,14 +3,19 @@
 module Main where
 
 import           Control.Lens hiding ( value )
+import           Data.Maybe
 import           Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
 import           Data.Time.Calendar
+import           Data.Time.Calendar.WeekDate
 import           Data.Time.Clock
+import           Data.Time.Format
 import           Data.Time.LocalTime
 import           Data.Time.Recurrence
 import           Prelude hiding (filter)
 import           Shelly
+import           System.Environment
+import           System.Locale
 import           Text.Printf
 
 default (Integer, Text)
@@ -34,17 +39,27 @@ countWorkDays beg end =
     length $ takeWhile (<= end) $ starting beg $
         recur daily >==> filter (WeekDays [Monday .. Friday])
 
+isWeekendDay :: Day -> Bool
+isWeekendDay day = let (_,_,dow) = toWeekDate day
+                   in dow == 6 || dow == 7
+
 main :: IO ()
 main = shelly $ silently $ do
+    args  <- liftIO $ getArgs
+
     data1 <- run "org2tc" ["/Users/johnw/doc/Tasks/todo.txt"] -|-
              run "ledger" ["-f", "-", "print", "complete"]
     data2 <- run "org2tc" ["/Users/johnw/doc/Tasks/FPComplete.txt"] -|-
              run "ledger" ["-f", "-", "print"]
 
     setStdin (T.append data1 data2)
-    balance <- run "ledger" ["-f", "-", "-p", "this month", "--base", "bal"]
+    balance <- run "ledger" ["-f", "-", "--base"
+                           , "-p", if null args
+                                   then "this month"
+                                   else T.pack (head args), "bal"]
 
     let debug   = False
+        realHrs' = 0.0 :: Float
         realHrs =
             case T.lines balance of
                 [] -> 0.0 :: Float
@@ -54,31 +69,38 @@ main = shelly $ silently $ do
         -- jww (2013-01-16): FP Complete reduces the work month by one day
         adjustedHrs = realHrs + 8.0
 
-    now <- liftIO $ getZonedTime
+    now <- if null args
+          then liftIO $ zonedTimeToLocalTime <$> getZonedTime
+          else do
+              dateString <-
+                  run "ledger" ["eval", "--now", T.pack (head args), "today"]
+              return . fromJust $
+                  parseTime defaultTimeLocale "%Y/%m/%d" (T.unpack dateString)
 
-    -- let firstDay  = ZonedTime (LocalTime (fromGregorian 2013 1 1) midday)
-    --                           (TimeZone (-360) True "CDT")
-    --     secondDay  = ZonedTime (LocalTime (fromGregorian 2013 1 2) midday)
-    --                           (TimeZone (-360) True "CDT")
-    --     thirdDay  = ZonedTime (LocalTime (fromGregorian 2013 1 3) midday)
-    --                           (TimeZone (-360) True "CDT")
+    let firstDay  = LocalTime (fromGregorian 2013 1 1) midday
+        secondDay = LocalTime (fromGregorian 2013 1 2) midday
+        thirdDay  = LocalTime (fromGregorian 2013 1 3) midday
 
-    let today     = toGregorian (localDay (zonedTimeToLocalTime now))
+    let today     = toGregorian (localDay now)
+        currHour  = fromIntegral (todHour (localTimeOfDay now)) / 3.0 :: Float
         (beg,end) = monthRange (fromIntegral (today^._1))
                                (fromIntegral (today^._2))
         workHrs   = getHours beg end
-        targetHrs = getHours beg (localTimeToUTC (hoursToTimeZone 0)
-                                                 (zonedTimeToLocalTime now))
+        targetHrs = if null args
+                    then (if isWeekendDay (localDay now) then 0 else currHour) +
+                         getHours beg (localTimeToUTC (hoursToTimeZone 0) now)
+                    else workHrs
         discrep   = adjustedHrs - targetHrs
         indicator = if discrep < 0
                     then "\ESC[31m↓\ESC[0m"
                     else "\ESC[32m↑\ESC[0m"
-        paceMark  = (adjustedHrs / workHrs) * 100.0
+        paceMark  = (adjustedHrs * 100.0) / workHrs
 
     when debug $ liftIO $ do
         putStrLn $ "realHrs:     " ++ show realHrs
         putStrLn $ "adjustedHrs: " ++ show adjustedHrs
         putStrLn $ "today:       " ++ show today
+        putStrLn $ "currHour:    " ++ show currHour
         putStrLn $ "beg:         " ++ show beg
         putStrLn $ "end:         " ++ show end
         putStrLn $ "workHrs:     " ++ show workHrs
