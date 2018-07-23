0c8d62feeb461bb0a4da48cf23d5cc473b3d394d
John Wiegley <johnw@newartisans.com>
2018-07-23 10:24:07 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -14,30 +14,27 @@
 , mkDerivation ? null
 }:
 
-let
-  haskellPackages' = pkgs.haskell.packages.${compiler};
-
-  haskellPackages = pkgs.lib.fix (this: haskellPackages'.override {
-    overrides = with pkgs.haskell.lib; self: super: {
-      time-recurrence     = doJailbreak super.time-recurrence;
-      diagrams-builder    = doJailbreak super.diagrams-builder;
-      diagrams-cairo      = doJailbreak super.diagrams-cairo;
-      diagrams-contrib    = doJailbreak super.diagrams-contrib;
-      diagrams-core       = doJailbreak super.diagrams-core;
-      diagrams-graphviz   = doJailbreak super.diagrams-graphviz;
-      diagrams-lib        = doJailbreak super.diagrams-lib;
-      diagrams-postscript = doJailbreak super.diagrams-postscript;
-      diagrams-rasterific = doJailbreak super.diagrams-rasterific;
-      diagrams-svg        = doJailbreak super.diagrams-svg;
-      circle-packing      = doJailbreak super.circle-packing;
-      active              = doJailbreak super.active;
-      force-layout        = doJailbreak super.force-layout;
-      svg-builder         = doJailbreak super.svg-builder;
-    };
-  });
+let haskellPackages = pkgs.haskell.packages.${compiler};
 
 in haskellPackages.developPackage {
   root = ./.;
+
+  overrides = with pkgs.haskell.lib; self: super: {
+    time-recurrence     = doJailbreak super.time-recurrence;
+    diagrams-builder    = doJailbreak super.diagrams-builder;
+    diagrams-cairo      = doJailbreak super.diagrams-cairo;
+    diagrams-contrib    = doJailbreak super.diagrams-contrib;
+    diagrams-core       = doJailbreak super.diagrams-core;
+    diagrams-graphviz   = doJailbreak super.diagrams-graphviz;
+    diagrams-lib        = doJailbreak super.diagrams-lib;
+    diagrams-postscript = doJailbreak super.diagrams-postscript;
+    diagrams-rasterific = doJailbreak super.diagrams-rasterific;
+    diagrams-svg        = doJailbreak super.diagrams-svg;
+    circle-packing      = doJailbreak super.circle-packing;
+    active              = doJailbreak super.active;
+    force-layout        = doJailbreak super.force-layout;
+    svg-builder         = doJailbreak super.svg-builder;
+  };
 
   source-overrides = {
   };
