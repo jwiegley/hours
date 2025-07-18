c9630545b801736c9df5ca4d7ff7794b328b77a4
John Wiegley <johnw@newartisans.com>
2025-07-18 10:09:38 -0700

    changes

--- flake.nix
+++ flake.nix
@@ -9,35 +9,37 @@
 
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system overlays;
-        inherit (haskellNix) config;
-      };
-      flake = pkgs.hours.flake {
-      };
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          hours =
-            final.haskell-nix.project' {
-              src = ./.;
-              supportHpack = true;
-              compiler-nix-name = "ghc910";
-              shell.tools = {
-                cabal = {};
-                haskell-language-server = {};
-                # hlint = {};
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        flake = pkgs.hours.flake {
+        };
+        overlays = [ haskellNix.overlay
+          (final: prev: {
+            hours =
+              final.haskell-nix.project' {
+                src = ./.;
+                supportHpack = true;
+                compiler-nix-name = "ghc910";
+                shell = {
+                  tools = {
+                    cabal = {};
+                    haskell-language-server = {};
+                  };
+                  buildInputs = with pkgs; [
+                    pkg-config
+                  ];
+                  withHoogle = true;
+                };
               };
-              shell.buildInputs = with pkgs; [
-                pkg-config
-              ];
-            };
-        })
-      ];
-    in flake // {
-      packages.default = flake.packages."hours:exe:process-hours";
-      devShells.default = flake.devShells.default // {
-        withHoogle = true;
-      };
-    });
+          })
+        ];
+      in flake // {
+        packages.default = flake.packages."hours:exe:process-hours";
+        devShells.default = flake.devShells.default // {
+          withHoogle = true;
+        };
+      });
 }
